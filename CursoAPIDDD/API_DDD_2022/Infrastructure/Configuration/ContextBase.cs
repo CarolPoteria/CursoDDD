9b0f00e6684c0667276ece2e708ecdb3e71c2b8d
Caroline Santos Poteria <c.santos.poteria@avanade.com>
2022-10-06 17:06:53 -0300

    Feat: Criação de projeto

---
+++ CursoAPIDDD/API_DDD_2022/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options): base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=localhost;Initial Catalog=API_DDD_2022;Persist Security Info=True;User ID=sa;Password=Admin1234";
+        }
+    }
+}
